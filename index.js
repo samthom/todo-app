48de6c2544ee19ca5b2c480eae24b8f13f507e14
sam338 <sam@three38inc.com>
2021-12-02 22:30:26 +0530

    first commit :tada:

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const port = process.env.PORT || 8000;
+// Create express app instance
+const app = express();
+// Simple index route
+app.get("/", (req, res) => res.send("Hello world"));
+// Start server
+app.listen(port, () => console.log(`Server started at port ${port}`))
